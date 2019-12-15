d49c4d2862183392d2e90600b0d4e25093c90662
liuzhongpo <lzp9294@163.com>
2019-12-15 11:03:24 +0800

    稀疏数组转二位数组

--- chapter01-data-structure/src/main/java/com/lzplzp/data/structure/array/sparse/SparseArray.java
+++ chapter01-data-structure/src/main/java/com/lzplzp/data/structure/array/sparse/SparseArray.java
@@ -31,31 +31,42 @@ public class SparseArray {
         }
 
         //2.定义
-        int[][] sparseArray = new int[sum+1][3];
-        sparseArray[0][0]=11;
-        sparseArray[0][1]=11;
-        sparseArray[0][2]=sum;
+        int[][] sparseArray = new int[sum + 1][3];
+        sparseArray[0][0] = 11;
+        sparseArray[0][1] = 11;
+        sparseArray[0][2] = sum;
 
         //3.赋值有效值
         int count = 0;
         for (int i = 0; i < chessArray.length; i++) {
             for (int j = 0; j < chessArray[i].length; j++) {
-                if(chessArray[i][j]!=0) {
+                if (chessArray[i][j] != 0) {
                     count++;
-                    sparseArray[count][0]=i;
-                    sparseArray[count][1]=j;
-                    sparseArray[count][2]=chessArray[i][j];
+                    sparseArray[count][0] = i;
+                    sparseArray[count][1] = j;
+                    sparseArray[count][2] = chessArray[i][j];
                 }
             }
         }
 
         for (int[] array : sparseArray) {
             for (int i : array) {
-                System.out.print(i+"\t");
+                System.out.print(i + "\t");
             }
             System.out.println();
         }
 
         //稀疏数组转二维数组
+        int[][] chessArray2 = new int[sparseArray[0][0]][sparseArray[0][1]];
+        for (int i = 1; i < sparseArray.length; i++) {
+            chessArray2[sparseArray[i][0]][sparseArray[i][1]] = sparseArray[i][2];
+        }
+        for (int[] ints : chessArray2) {
+            for (int anInt : ints) {
+                System.out.print(anInt+"\t");
+            }
+            System.out.println();
+        }
+
     }
 }
